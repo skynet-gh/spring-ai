1ca515f95c451ff964f93928dbf8b1f1db9ae39e
skynet <276247+skynet-gh@users.noreply.github.com>
2021-06-12 10:09:01 -0400

    Start describing world in more detail

--- src/clj/skynet.clj
+++ src/clj/skynet.clj
@@ -1,9 +1,30 @@
 (ns skynet
   (:require
     [chime.core :as chime]
-    [skynet.old :as old]
+    [clojure.core.protocols]
+    [clojure.datafy :refer [datafy]]
     [java-time :as jt]
-    [taoensso.timbre :as log]))
+    [skynet.old :as old]
+    [skynet.util :as u]
+    [taoensso.timbre :as log])
+  (:import
+    (com.springrts.ai.oo.clb Unit)))
+
+
+#_
+(extend-protocol clojure.core.protocols/Datafiable
+  Unit
+  (datafy [x]
+    {:use-metal (.getRes)}))
+
+
+(defn unit-data [resources unit]
+  (let [unit-def (.getDef unit)]
+    {:name (.getName unit-def)
+     :pos (.getPos unit)
+     :build-speed (.getBuildSpeed unit-def)
+     :use-metal (.getResourceUse unit (:metal resources))
+     :use-energy (.getResourceUse unit (:energy resources))}))
 
 
 (defn handle-error [exception]
@@ -11,16 +32,49 @@
   true)
 
 
+(def ignore-idle
+  #{"armmex" "armsolar" "armwin" "armmakr" "armadvsol" "armmoho" "armmmkr" "armfus"})
+
+
+(defn world
+  "Returns data about the world obtained from state."
+  [{:keys [callback avg-wind resources metal-spots] :as state}]
+  (let [map-obj (.getMap callback)
+        team-units (.getTeamUnits callback)
+        builder-units (filter (comp #(.isBuilder %) #(.getDef %)) team-units)
+        mexes (filter (comp pos? #(.getExtractsResource % (:metal resources)) #(.getDef %)) team-units)
+        mex-spots (->> mexes
+                       (map
+                         (fn [mex]
+                           {(.getResourceMapSpotsNearest map-obj (.getPos mex))
+                            {(.getName (.getdef mex)) mex}}))
+                       (merge-with merge))
+        metal-details (into {}
+                        (map
+                          (fn [pos]
+                            [pos {::mexes (get mex-spots pos)}])
+                          metal-spots))]
+    {::economy (old/economy state)
+     ::start-pos (.getStartPos map-obj)
+     ::builders builder-units
+     ::builders-data (map (partial unit-data resources) builder-units)
+     ::team-units team-units
+     ::metal-details metal-details}))
+
+
 (defn run [state-atom]
   (log/info "Async planner")
   (log/info "Looking for idle units")
   (let [state @state-atom
         team-units (.getTeamUnits (:callback state))
-        idle-units (filter (comp empty? #(.getCurrentCommands %)) team-units)]
+        ;do-things-units (filter (comp #(or (.isBuilder %) (.isAbleToAttack %)) #(.getDef %)) team-units)
+        do-things-units (remove (comp ignore-idle #(.getName %) #(.getDef %)) team-units)
+        idle-units (filter (comp empty? #(.getCurrentCommands %)) do-things-units)]
+    (log/debug (u/pprint-str (world state)))
     (dorun
       (map
         (fn [unit]
-          (log/info "Giving" unit "a job")
+          (log/info "Giving" unit "type" (.getName (.getDef unit)) "a job")
           (old/assign-unit state unit))
         idle-units))))
 
--- test/clj/skynet_test.clj
+++ test/clj/skynet_test.clj
@@ -1,7 +1,11 @@
 (ns skynet-test
   (:require
+    [clojure.string :as string]
     [clojure.test :refer [deftest is testing]]
-    [skynet]))
+    [skynet])
+  (:import
+    (com.springrts.ai.oo AIFloat3)
+    (com.springrts.ai.oo.clb Economy Map OOAICallback Resource)))
 
 
 (deftest init
@@ -10,3 +14,98 @@
       (is (instance? java.lang.AutoCloseable chimer))
       (finally
         (.close chimer)))))
+
+
+(def initial-economy
+  {:metal
+   {:received 0.0,
+    :excess 0.0,
+    :sent 0.0,
+    :usage 0.0,
+    :share 0.99,
+    :current 1000.0,
+    :storage 1000.0,
+    :pull 0.0,
+    :income 0.0},
+   :energy
+   {:received 0.0,
+    :excess 0.0,
+    :sent 0.0,
+    :usage 0.0,
+    :share 0.95,
+    :current 1000.0,
+    :storage 1000.0,
+    :pull 0.0,
+    :income 0.0}})
+
+(def later-economy
+  {:metal
+   {:received 0.0,
+    :excess 0.0,
+    :sent 0.0,
+    :usage 6.842015,
+    :share 0.99,
+    :current 85.62922,
+    :storage 2250.0,
+    :pull 6.842015,
+    :income 5.708743},
+   :energy
+   {:received 0.0,
+    :excess 0.0,
+    :sent 0.0,
+    :usage 97.722534,
+    :share 0.95,
+    :current 260.60312,
+    :storage 1250.5,
+    :pull 97.722534,
+    :income 92.74297}})
+
+(defn get-initial [resource metric]
+  (when resource
+    (get-in initial-economy [(keyword (string/lower-case (.getName resource))) metric])))
+
+
+(deftest world
+  (testing "initial"
+    (let [map-obj (reify Map
+                    (getStartPos [this]
+                      (AIFloat3. 1 1 1))
+                    (getResourceMapSpotsNearest [this resource pos]
+                      (case (.getName resource)
+                        "Metal" (AIFloat3. 0 0 0)
+                        "Energy" (AIFloat3. 0 0 0))))
+          economy (reify Economy
+                    (getCurrent [this resource]
+                      (get-initial resource :current))
+                    (getIncome [this resource]
+                      (get-initial resource :income))
+                    (getUsage [this resource]
+                      (get-initial resource :usage))
+                    (getStorage [this resource]
+                      (get-initial resource :storage))
+                    (getPull [this resource]
+                      (get-initial resource :pull))
+                    (getShare [this resource]
+                      (get-initial resource :share))
+                    (getSent [this resource]
+                      (get-initial resource :sent))
+                    (getReceived [this resource]
+                      (get-initial resource :received))
+                    (getExcess [this resource]
+                      (get-initial resource :excess)))
+          callback (reify OOAICallback
+                     (getMap [this]
+                       map-obj)
+                     (getTeamUnits [this]
+                       [])
+                     (getEconomy [this]
+                       economy))
+          state {:callback callback
+                 :resources {:metal (reify Resource
+                                      (getName [this]
+                                        "Metal"))
+                             :energy (reify Resource
+                                       (getName [this]
+                                         "Energy"))}}]
+      (is (= {}
+             (skynet/world state))))))
